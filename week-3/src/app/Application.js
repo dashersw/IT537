3df46f791eada782633158aff470b60a22da0080
Armagan Amcalar <armagan@amcalar.com>
2017-02-18 12:07:12 +0300

    Implement a simple HTML form with event listeners

--- week-3/src/app/Application.js
+++ week-3/src/app/Application.js
@@ -1,5 +1,26 @@
+import dom from '../../lib/dom';
+
 export default class Application {
     constructor() {
         console.log('Hello world!');
+
+        var form = dom.$('form');
+        var emailField = form.email;
+        var passwordField = form.password;
+        var output = dom.$('#output');
+        var submitButton = dom.$('input[name=submit]');
+
+        form.addEventListener('submit', function(e) {
+            var password = passwordField.value;
+            var email = emailField.value;
+
+            var errorOutput = '';
+            if (/\d{2}/.test(password) == false)
+                errorOutput = 'The password should contain exactly two digits';
+
+            output.innerText = errorOutput;
+
+            e.preventDefault();
+        });
     }
 }
