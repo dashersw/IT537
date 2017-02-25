38d40c141d175d0f5f878a7d838c455f92d467e0
Armagan Amcalar <armagan@amcalar.com>
2017-02-25 12:04:11 +0300

    Use ES6 string syntax for list item templates

--- week-4/src/views/shows/list-item.js
+++ week-4/src/views/shows/list-item.js
@@ -8,10 +8,12 @@ class ListItem extends erste.Component {
     template() {
         var imgFile = this.show['images']['fanart'].split('/').slice(-1);
 
-        return '<list-item id="' + this.id + '" data-show-id="' + this.show['_id'] + '"' +
-            'style="background-image: url(static/img/cover/' + imgFile + ')">' +
-            '<show-title>' + this.show['title'] + '</show-title>' +
-            '</list-item>';
+        return `
+<list-item id="${this.id}" data-show-id="${this.show['_id']}"
+style="background-image: url(static/img/cover/${imgFile}">
+    <show-title>${this.show['title']}</show-title>
+</list-item>
+`;
     };
 
 }
